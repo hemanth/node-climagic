65830b339bc459faaaff0f22997967c7513b3500
hemanth.hm <hemanth.hm@gmail.com>
2012-05-23 22:00:13 +0530

    fixed cli function

--- climagic.js
+++ climagic.js
@@ -1,7 +1,7 @@
 nom = require('nom');
 
-cmd = function(){nom("http://climagic.org", function(err, $) {
-            console.log($('tt').text());
-            });
-});
+cmd = function(){nom("http://climagic.org", function(err, $){
+                console.log($('tt').text());
+      });
+}
 
